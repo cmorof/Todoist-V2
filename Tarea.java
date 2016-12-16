5d723ec6c4aa6545d906d52aaa231a92d33a6b92
Carlos Moro <carlos.moro.dam1@iessanandres.com>
2016-12-16 14:21:19 +0100

    método actualizado

--- Tarea.java
+++ Tarea.java
@@ -3,11 +3,13 @@ public class Tarea
 {
     private String descripcion;
     private Boolean terminada;
+    private int prioridad;
     
     public Tarea(String nombre)
     {
         descripcion = nombre;
         terminada = false;
+        prioridad = 0;
     }
     
     public String mostrarTarea()
@@ -23,5 +25,26 @@ public class Tarea
     public void marcarCompleta()
     {
         terminada = true;
+    }
+    
+    public String toString()
+    {
+        String textoADevolver = "";
+        if (terminada)
+        {
+            textoADevolver += "HECHA ";
+        }
+        textoADevolver += descripcion;
+        return textoADevolver;
+    }
+    
+     /**
+     * Método para establecer prioridad para una tarea del 0 al 5.
+     */
+    public void establecerPrioridad(int numeroPrioridad)
+    {
+        if (numeroPrioridad >= 0 && numeroPrioridad <=5){
+            prioridad = numeroPrioridad;
+        }
     }
 }
